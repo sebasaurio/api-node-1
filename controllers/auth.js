0b8f6c3cac6195c66613cdc50264baa070032b62
Sebastian <sebastian.b.c.d@gmail.com>
2022-10-17 22:06:58 -0300

    avanzando login google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,54 @@
-import {response} from 'express'
-import bcryptjs from 'bcryptjs'
-import User from '../models/user'
-import { generateJWT } from '../helpers/generate-jwt'
+import { response } from 'express';
+import bcryptjs from 'bcryptjs';
+import User from '../models/user';
+import { generateJWT } from '../helpers/generate-jwt';
+import { verify } from '../helpers/google-verify';
 
-export const login = async(req, res = response) => {
-    const {email, password} = req.body
-    try{
-        //verify email
-        const user = await User.findOne({email})
-        
-        if(!user) {
-            return res.status(400).json({ message: 'User not exist'})
-        }
-        //verify user
-        if(!user.status) {
-            return res.status(400).json({ message: 'User is disabled' })
-        }
-        //verify password
-        if(!bcryptjs.compareSync(password, user.password)) {
-            return res.status(400).json({ message: 'Invalid password' })
-        }
-        
-        //generate jwt
-        const token = await generateJWT(user.id)
-        console.log(token)
-        return res.json({
-            user, 
-            token
-        })
+export const login = async (req, res = response) => {
+  const { email, password } = req.body;
+  try {
+    //verify email
+    const user = await User.findOne({ email });
 
-    }catch(error){
-        console.log(error)
-        return res.status(500).json({
-            message: 'Error on login'
-        })
+    if (!user) {
+      return res.status(400).json({ message: 'User not exist' });
     }
-}
+    //verify user
+    if (!user.status) {
+      return res.status(400).json({ message: 'User is disabled' });
+    }
+    //verify password
+    if (!bcryptjs.compareSync(password, user.password)) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    //generate jwt
+    const token = await generateJWT(user.id);
+    console.log(token);
+    return res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Error on login',
+    });
+  }
+};
+
+export const googleSignIn = async (req, res = response) => {
+  try {
+    const { id_token } = req.body;
+    const googleUser = await verify(id_token);
+    console.log(googleUser);
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Error on login with google sign in',
+    });
+  }
+
+  res.json({
+    message: 'Ok',
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,24 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { login } from '../controllers/auth';
+import { login, googleSignIn } from '../controllers/auth';
 import { validateFields } from '../middlewares/validator';
 
-const router = Router()
+const router = Router();
 
-router.post('/login',[
-    check('email','email is required').isEmail(),
+router.post(
+  '/login',
+  [
+    check('email', 'email is required').isEmail(),
     check('password', 'password is required').not().isEmpty(),
-    validateFields
-], login)
+    validateFields,
+  ],
+  login
+);
+
+router.post(
+  '/google',
+  [check('id_token', 'token is needed').not().isEmpty()],
+  googleSignIn
+);
 
 export default router;
